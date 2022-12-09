75ed4f8b0d402e11f0895caf5c48205c9f444b43
justteck <justteck@yandex.ru>
2022-12-09 15:46:59 +0300

    module_3_lesson_02_fix

--- lesson_2/js/script.js
+++ lesson_2/js/script.js
@@ -4,8 +4,19 @@ const p = document.createElement('p');
 document.body.append(input);
 document.body.append(p);
 
-input.addEventListener('input', () => {
-  setTimeout(() => {
-    p.textContent = input.value;
-  }, 300);
-});
+const showText = () => {
+  console.log(input.value);
+  p.textContent = input.value;
+};
+
+const debounce = (func, delayTime) => {
+  let timeout;
+  return function doWithDelay() {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delayTime);
+  };
+};
+
+const debounceShowText = debounce(showText, 700);
+
+input.addEventListener('keyup', debounceShowText);
